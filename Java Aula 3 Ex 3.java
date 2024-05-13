efd724d245cd29b57c3e143c2e23de33358ec66e
EnzoWarner <enzo_warner@hotmail.com>
2024-05-13 03:41:21 -0300

    first commit

---
+++ Java Aula 3 Ex 3.java
@@ -0,0 +1,9 @@
+public class EX3_3 {
+    public static void main (String[] args) {
+    int i = 10;
+    int n = i++;
+    int n1 = ++i;
+    System.out.println(n);
+    System.out.println(n1);
+}
+}
